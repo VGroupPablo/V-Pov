d102c14cf2b48da7393b2f2ec2d61a29ed2b72b4
VGroupPablo <129444988+VGroupPablo@users.noreply.github.com>
2023-04-15 19:31:57 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,3 +2,5 @@ Config = {}
 
 Config.Enable = true -- Boolean
 Config.Disableautosprint = true -- Boolean
+Config.shakestill = 0.20 -- From 0.01 to 0.99 / This is when u stand still
+Config.shakemovement = 0.99 -- From 0.01 to 0.99 / This is when u are moving
