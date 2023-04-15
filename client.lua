30839a39122d0ac71026bfea2a93afcf28b8f4c0
VGroupPablo <129444988+VGroupPablo@users.noreply.github.com>
2023-04-15 19:26:58 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,15 +1,24 @@
 Citizen.CreateThread(function()
-	while true do
+	while Config.Enable do
 		Citizen.Wait(1)
 		local camera = GetFollowPedCamViewMode()
 		local ped = PlayerPedId()
+		local player = PlayerId()
+		local cam = false
 		DisableControlAction(2, 0, true)
 		if camera ~= 4 then
 			SetFollowPedCamViewMode(4)
+			cam = true 
+		else
+				
 		end
-		if IsControlJustPressed(2, 32) or IsControlJustPressed(2, 33) or IsControlJustPressed(2, 34) or IsControlJustPressed(2, 35) then
-			if not IsControlJustPressed(2, 21) then
-				SetControlNormal(2, 21, 1.0)
+		if Config.Disableautosprint then
+			if IsControlJustPressed(2, 32) or IsControlJustPressed(2, 33) or IsControlJustPressed(2, 34) or IsControlJustPressed(2, 35) then
+				if not IsControlJustPressed(2, 21) then
+					SetControlNormal(2, 21, 1.0)
+				end
+			else
+
 			end
 		end
 	end
